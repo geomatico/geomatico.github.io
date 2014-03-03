d69a2c396f49c444aefa5fb583c1f3b6e429a422
Oscar Fonts <oscar.fonts@gmail.com>
2014-03-03 12:42:40 +0100

    Updated Leaflet Map. Closes #15

--- javascript/mapa.js
+++ javascript/mapa.js
@@ -1,86 +1,47 @@
-// Define your locations: HTML content for the info window, latitude, longitude
-    var locations = [
-      ['<h4>Bondi Beach</h4>', -33.890542, 151.274856],
-      ['<h4>Coogee Beach</h4>', -33.923036, 151.259052],
-      ['<h4>Cronulla Beach</h4>', -34.028249, 151.157507],
-      ['<h4>Manly Beach</h4>', -33.80010128657071, 151.28747820854187],
-      ['<h4>Maroubra Beach</h4>', -33.950198, 151.259302]
-    ];
-    
-    // Setup the different icons and shadows
-    var iconURLPrefix = 'http://maps.google.com/mapfiles/ms/icons/';
-    
-    var icons = [
-      iconURLPrefix + 'red-dot.png',
-      iconURLPrefix + 'green-dot.png',
-      iconURLPrefix + 'blue-dot.png',
-      iconURLPrefix + 'orange-dot.png',
-      iconURLPrefix + 'purple-dot.png',
-      iconURLPrefix + 'pink-dot.png',      
-      iconURLPrefix + 'yellow-dot.png'
-    ]
-    var icons_length = icons.length;
-    
-    
-    var shadow = {
-      anchor: new google.maps.Point(15,33),
-      url: iconURLPrefix + 'msmarker.shadow.png'
-    };
+// The map
+var map = L.map('map', {
+  crs: L.CRS.EPSG4326,
+  scrollWheelZoom: false
+}).setView([49.25, 2.25], 5);
 
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 10,
-      center: new google.maps.LatLng(-37.92, 151.25),
-      mapTypeId: google.maps.MapTypeId.ROADMAP,
-      mapTypeControl: false,
-      streetViewControl: false,
-      panControl: false,
-      zoomControlOptions: {
-         position: google.maps.ControlPosition.LEFT_BOTTOM
-      }
-    });
+// The base layer
+L.tileLayer('http://otile{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png', {
+  subdomains: '1234',
+  minZoom: 2,
+  maxZoom: 17,
+  attribution: 'Map data © <a href="http://www.openstreetmap.org" target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a href="http://www.mapquest.com/" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png">'
+}).addTo(map);
 
-    var infowindow = new google.maps.InfoWindow({
-      maxWidth: 160
-    });
+// The marker data
+var marker_data = [{
+  name: "Fernando González",
+  img: "fernando",
+  location: [55.06328201, 11.57512665]
+},{
+  name: "Oscar Fonts",
+  img: "oscar",
+  location: [46.26666498, 2.25481510]
+},{
+  name: "Micho García",
+  img: "micho",
+  location: [46.68171787, -8.72563362]
+},{
+  name: "Víctor González",
+  img: "victor",
+  location: [43.00245665, -0.38314819]
+},{
+  name: "Jorge Arévalo",
+  img: "jorge",
+  location: [44.25410842, -3.69724274]
+}];
 
-    var marker;
-    var markers = new Array();
-    
-    var iconCounter = 0;
-    
-    // Add the markers and infowindows to the map
-    for (var i = 0; i < locations.length; i++) {  
-      marker = new google.maps.Marker({
-        position: new google.maps.LatLng(locations[i][1], locations[i][2]),
-        map: map,
-        icon : icons[iconCounter],
-        shadow: shadow
-      });
-
-      markers.push(marker);
-
-      google.maps.event.addListener(marker, 'click', (function(marker, i) {
-        return function() {
-          infowindow.setContent(locations[i][0]);
-          infowindow.open(map, marker);
-        }
-      })(marker, i));
-      
-      iconCounter++;
-      // We only have a limited number of possible icon colors, so we may have to restart the counter
-      if(iconCounter >= icons_length){
-      	iconCounter = 0;
-      }
-    }
-
-    function AutoCenter() {
-      //  Create a new viewpoint bound
-      var bounds = new google.maps.LatLngBounds();
-      //  Go through each...
-      $.each(markers, function (index, marker) {
-        bounds.extend(marker.position);
-      });
-      //  Fit these bounds to the map
-      map.fitBounds(bounds);
-    }
-    AutoCenter();
+for(var i = 0; i < marker_data.length; i++) {
+  var member = marker_data[i];
+  L.marker(member.location, {
+    icon: L.icon({
+      iconUrl: "imagenes/retrato_mapa_" + member.img + ".png",
+      iconAnchor: [25, 25],
+      popupAnchor: [0, -20]
+    })
+  }).addTo(map).bindPopup("<h4 style='white-space:nowrap;'>" + member.name + "</h4>");
+}
