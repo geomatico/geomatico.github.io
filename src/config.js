ef37189175b1453f4f3d81bdf2ffbe05071b94da
Francisco Pérez Sampayo <francisco.perez@geomatico.es>
2021-06-16 10:25:46 +0200

    minimize images

--- src/config.js
+++ src/config.js
@@ -1,27 +1,27 @@
 export const CONFIG = {
   projects: [
     {
-      img: 'img/1km.png',
+      img: 'img/1km.jpg',
       title: '1km.geomatico.es',
       url: 'https://1km.geomatico.es',
     },
     {
-      img: 'img/mapa-catastro-3d.png',
+      img: 'img/mapa-catastro-3d.jpg',
       title: 'Catastro 3D',
       url: 'https://labs.geomatico.es/mapa-catastro-3d',
     },
     {
-      img: 'img/pirineos-3d.png',
+      img: 'img/pirineos-3d.jpg',
       title: 'Pirineos 3D',
       url: 'https://labs.geomatico.es/pirineos-3d',
     },
     {
-      img: 'img/satagro.png',
+      img: 'img/satagro.jpg',
       title: 'SatAgro',
       url: 'https://app.satagro.pl/es/login/#dashboard',
     },
     {
-      img: 'img/otp.png',
+      img: 'img/otp.jpg',
       title: 'Otp.js',
       url: 'https://labs.geomatico.es/otp-react-redux',
     }
