087e8e75779245631c39ca24560e80dd853160f9
Oscar Fonts <oscar.fonts@geomati.co>
2021-06-22 09:28:52 +0200

    Back to PNG, scaled & quantized

--- src/config.js
+++ src/config.js
@@ -1,27 +1,27 @@
 export const CONFIG = {
   projects: [
     {
-      img: 'img/1km.jpg',
+      img: 'img/1km.png',
       title: '1km.geomatico.es',
       url: 'https://1km.geomatico.es',
     },
     {
-      img: 'img/mapa-catastro-3d.jpg',
+      img: 'img/mapa-catastro-3d.png',
       title: 'Catastro 3D',
       url: 'https://labs.geomatico.es/mapa-catastro-3d',
     },
     {
-      img: 'img/pirineos-3d.jpg',
+      img: 'img/pirineos-3d.png',
       title: 'Pirineos 3D',
       url: 'https://labs.geomatico.es/pirineos-3d',
     },
     {
-      img: 'img/satagro.jpg',
+      img: 'img/satagro.png',
       title: 'SatAgro',
       url: 'https://app.satagro.pl/es/login/#dashboard',
     },
     {
-      img: 'img/otp.jpg',
+      img: 'img/otp.png',
       title: 'Otp.js',
       url: 'https://labs.geomatico.es/otp-react-redux',
     }
