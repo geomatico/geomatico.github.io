80c3698b9284a5486ff6ebe3925fda2aca9481a6
martitasea <marta.pulido.asin@gmail.com>
2021-11-18 17:21:35 +0100

    Arreglado efecto hover, responsive

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -8,36 +8,45 @@ import makeStyles from '@mui/styles/makeStyles';
 import useMediaQuery from '@mui/material/useMediaQuery';
 
 const useStyles = makeStyles(() => ({
-  gridList: {
-    margin: '0 auto!important',
+  imageContent: {
+    width: '100%',
     height: '100%',
-    width: ({widescreen}) => widescreen ? '50vw' : '100%'
-  },
-  image: {
-    width: ({widescreen}) => widescreen ? '25vw' : '100%',
-    height: '100%',
+    overflow: 'hidden',
     objectFit: 'cover',
     transition: 'transform .4s',
     '&:hover': {
-      transform: 'scale(1.3)',
-      transformOrigin: '50% 50%'
+      '& img': {
+        transform: 'scale(1.3)',
+        transformOrigin: '50% 50%',
+      }
     }
-  }
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    objectFit: 'cover',
+    transition: 'transform .4s',
+  },
 }));
 
 const ProjectList = ({projects}) => {
-  const widescreen = useMediaQuery('(min-width: 770px)');
+  const widescreen = useMediaQuery('(min-width: 900px)');
   const classes = useStyles({widescreen});
 
-  return <ImageList cellHeight={350} className={classes.gridList}>
+  return <ImageList cols={widescreen? 3 : 2} gap={widescreen? 20 : 0} sx={{margin: {xs: 0, md: 4}}}>
     {projects.map((project) => (
       <ImageListItem key={project.img}>
-        <a href={project.url}>
-          <img src={project.img} alt={project.title} className={classes.image}/>
-        </a>
-        <ImageListItemBar
-          title={project.title}
-        />
+        <div className={classes.imageContent}>
+          <a href={project.url}>
+            <img
+              src={project.img}
+              alt={project.title}
+              className={classes.image}
+              loading="lazy"
+            />
+          </a>
+        </div>
+        <ImageListItemBar title={project.title} />
       </ImageListItem>
     ))}
   </ImageList>;
