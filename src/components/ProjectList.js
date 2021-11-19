d65c681f63686e01bba0a1faba710098841b7cfa
Oscar Fonts <oscar.fonts@gmail.com>
2021-11-19 09:31:34 +0100

    FRONT-6 | Mui5, geocomponents 1.0.0 (#32)
    * mui5
    * Apuntando a geocomponents 1.0.0
    * Arreglado efecto hover, responsive
    * Eliminado useStyles en favor de sx prop
    * Readd package-lock
    
    Co-authored-by: martitasea <marta.pulido.asin@gmail.com>

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,52 +1,46 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import GridListTile from '@material-ui/core/GridListTile';
-import GridListTileBar from '@material-ui/core/GridListTileBar';
-import IconButton from '@material-ui/core/IconButton';
-import InfoIcon from '@material-ui/icons/Info';
-import GridList from '@material-ui/core/GridList';
-import {makeStyles} from '@material-ui/core/styles';
-import {useMediaQuery} from '@material-ui/core';
+import ImageListItem from '@mui/material/ImageListItem';
+import ImageListItemBar from '@mui/material/ImageListItemBar';
+import ImageList from '@mui/material/ImageList';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import Box from '@mui/material/Box';
 
-
-const useStyles = makeStyles(() => ({
-  gridList: {
-    margin: '0 auto!important',
+const ProjectList = ({projects}) => {
+  const widescreen = useMediaQuery('(min-width: 900px)');
+  const imageContent = {
+    width: '100%',
     height: '100%',
-    width: ({widescreen}) => widescreen ? '50vw' : '100%'
-  },
-  image: {
-    width: ({widescreen}) => widescreen ? '25vw' : '100%',
-    height: '100%',
+    overflow: 'hidden',
     objectFit: 'cover',
     transition: 'transform .4s',
     '&:hover': {
-      transform: 'scale(1.3)',
-      transformOrigin: '50% 50%'
+      '& img': {
+        transform: 'scale(1.3)',
+        transformOrigin: '50% 50%',
+      }
     }
-  },
-  
-}));
+  };
 
-const ProjectList = ({projects}) => {
-  const widescreen = useMediaQuery('(min-width: 770px)');
-  const classes = useStyles({widescreen});
-  
-  return(
-    <GridList cellHeight={350} className={classes.gridList}>
-      {projects.map((project) => (
-        <GridListTile key={project.img}>
+  return <ImageList cols={widescreen? 3 : 2} gap={widescreen? 20 : 0} sx={{margin: {xs: 0, md: 4}}}>
+    {projects.map((project) => (
+      <ImageListItem key={project.img}>
+        <Box sx={imageContent}>
           <a href={project.url}>
-            <img src={project.img} alt={project.title} className={classes.image}/>
+            <img
+              src={project.img}
+              alt={project.title}
+              width='100%'
+              height='100%'
+              style={{objectFit: 'cover', transition: 'transform .4s'}}
+            />
           </a>
-          <GridListTileBar
-            title={project.title}
-          />
-        </GridListTile>
-      ))}
-    </GridList>
-  );
+        </Box>
+        <ImageListItemBar title={project.title} />
+      </ImageListItem>
+    ))}
+  </ImageList>;
 };
 
 ProjectList.propTypes = {
